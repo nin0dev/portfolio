a8684a9347d3a901c493361e3209e6024071fcd7
nin0dev <ninberber@gmail.com>
2026-03-26 15:03:21 -0400

    build

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  publicPath: '/portfolio/',
+  base: '/portfolio/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
